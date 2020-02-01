6df1d5e0ac3442da308c2fa9a2186b2858b93119
kamil.danielski@gmail.com <kamil.danielski@gmail.com>
2020-02-01 13:19:56 +0100

    Add two new types of loaders

---
+++ src/components/loaders/donut-spinner/donut-spinner.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './donut-spinner.styles.scss';
+
+const DonutSpinner = () => (
+    <div className="donut"></div>
+)
+
+export default DonutSpinner; 
---
+++ src/components/loaders/pulse-loader/pulse-loader.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './pulse-loader.styles.scss';
+
+const PulseLoader = () => (
+    <div class="ripple-loader">
+        <div></div>
+        <div></div>
+    </div>
+)
+
+export default PulseLoader; 
